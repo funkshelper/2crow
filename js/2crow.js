d11cd161041e0a4180c373f801c644f4e188f2d0
RobertStuff <robert_gludo@yahoo.com>
2015-02-06 14:18:10 +0800

    added error checking on 2crow.js and index.html tab1

--- js/2crow.js
+++ js/2crow.js
@@ -1,12 +1,37 @@
-$(function {
+$(function() {
 
 	$('#merchantAddress').click(function() {
-
+		var merchantAdd = $('#inputMerchantBTCAdd').val();
+		var amount = $('#inputAmount').val();
+		
+		if(!merchantAdd && !amount){
+			//if both fields are empty
+			$('#merchantBTCAdd').addClass('has-error');
+			$('#amount').addClass('has-error');
+			$("#multiSigErrorMsg").css("display", "block");
+			$("#multiSigErrorMsg").html('<span class="glyphicon glyphicon-exclamation-sign"></span> Please fill up the field!');
+		}else if(!merchantAdd){
+			//if merchant bitcoin address is empty
+			$('#merchantBTCAdd').addClass('has-error');
+			$('#amount').removeClass('has-error');
+			$("#multiSigErrorMsg").css("display", "block");
+			$("#multiSigErrorMsg").html('<span class="glyphicon glyphicon-exclamation-sign"></span> Please fill up the address field!');	
+		}else if(isNaN(amount)){
+			//if entered amount is not a number
+			$('#merchantBTCAdd').removeClass('has-error');
+			$('#amount').addClass('has-error');
+			$("#multiSigErrorMsg").css("display", "block");
+			$("#multiSigErrorMsg").html('<span class="glyphicon glyphicon-exclamation-sign"></span> Invalid amount!');	
+		}else{
+			$('#merchantBTCAdd').removeClass('has-error');
+			$('#amount').removeClass('has-error');
+			$("#multiSigErrorMsg").css("display", "none");
+		}
 	});
 
 	
 	$('#customerbegin').click(function() {
-
+		alert("hi");
 	});
 
 });
